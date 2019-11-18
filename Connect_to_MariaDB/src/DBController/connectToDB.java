76e76185c733afc1398f22e60ba04de7ea0614ab
JayJay-H <0911aza@naver.com>
2019-11-18 19:22:12 +0900

    DB접속 오류 해결

--- Connect_to_MariaDB/src/DBController/connectToDB.java
+++ Connect_to_MariaDB/src/DBController/connectToDB.java
@@ -10,19 +10,19 @@ public class connectToDB {
 		Connection 	conn = null;
 		Statement 	stmt = null;
 		try{
-            Class.forName("com.mysql.jdbc.Driver");
-            
-            String url = "jdbc:mysql://59.27.140.107:3306/term_project_seven";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://59.27.140.107:3306/term_project_seven?characterEncoding=UTF-8&serverTimezone=UTC&useSSL=false";
             conn = DriverManager.getConnection(url, "termSeven", "sevenmillion");
 
             stmt = conn.createStatement();
         }
-        catch( ClassNotFoundException e){
-            System.out.println("Driver Loading Failed");
-        }
         catch( SQLException e){
             System.out.println("Error " + e);
-        }
+        } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return stmt;
 	}
