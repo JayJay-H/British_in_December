77422e64b989d1a317a2749d7188031deb86d59e
Isaac-Lee <yy0221ss@gmail.com>
2019-11-29 17:50:03 +0900

    수정

--- Connect_to_MariaDB/src/Client/java/Client.java
+++ Connect_to_MariaDB/src/Client/java/Client.java
@@ -7,25 +7,24 @@ import Server_Class.ScooterManagement.scooterManagement;
 public class Client {
 	static boolean islogin = false;
 	boolean isUsing = false;
-	
+
 	static void Login(String inputID, String inputPassword) {
 		String Loginstatus = authMember.authenticateMember(inputID, inputPassword);
-        if (Loginstatus.equals("0")){
-            islogin = true;
-        } else {
-            islogin = false;
-        }
-        }
+		if (Loginstatus.equals("0")) {
+			islogin = true;
+		} else {
+			islogin = false;
+		}
 	}
-	
+
 	static String findScooterList() throws SQLException {
 		return scooterManagement.findScooterList();
 	}
-	
+
 	static String findScooter(String ID) throws SQLException {
 		return scooterManagement.findScooter(ID);
 	}
-	
+
 	static int getNumberOfCanUseScooter() throws SQLException {
 		return scooterManagement.getNumberOfCanUseScooter();
 	}
