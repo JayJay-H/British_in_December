1f40341832ad4de1d1c1bb938f5da9048cd4a090
2T_HONG <lth1283910@gmail.com>
2019-11-30 13:00:01 +0900

    #25 태홍 창 이동 절반 구현(절대경로 지정하는 방법 등)

--- Connect_to_MariaDB/src/Manager/Java/Controllers/ScooterInfoGuiController.java
+++ Connect_to_MariaDB/src/Manager/Java/Controllers/ScooterInfoGuiController.java
@@ -1,21 +1,76 @@
 package Manager.Java.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class ScooterInfoGuiController {
 
-	@FXML Button backButton;
-	@FXML Button confirmScooterListButton;
-	@FXML Button confirmScooterInformationButton;
-	@FXML Button findScooterLocationButton;
+	@FXML
+	Button backButton;
+	@FXML
+	Button confirmScooterListButton;
+	@FXML
+	Button confirmScooterInformationButton;
+	@FXML
+	Button findScooterLocationButton;
 
-	@FXML public void backButtonHandler() {}
+	@FXML
+	public void backButtonHandler(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Manager/Resource/View/ManagerGui.fxml")); // login화면으로
+																												// 돌아가도록
+																												// 한다.
+			Scene scene = new Scene(root);
+			;
+			Stage primaryStage = (Stage) backButton.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-	@FXML public void confirmScooterListButtonHandler() {}
+	@FXML
+	public void confirmScooterListButtonHandler(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Manager/Resource/View/ScooterManagementGui.fxml"));
+			Scene scene = new Scene(root);
+			;
+			Stage primaryStage = (Stage) backButton.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-	@FXML public void confirmScooterInformationButtonHandler() {}
+	@FXML
+	public void confirmScooterInformationButtonHandler(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Manager/Resource/View/ScooterInformation.fxml"));
+			Scene scene = new Scene(root);
+			;
+			Stage primaryStage = (Stage) backButton.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-	@FXML public void findScooterLocationButtonHandler() {}
+	@FXML
+	public void findScooterLocationButtonHandler(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Manager/Resource/View/ScooterLocationGui.fxml"));
+			Scene scene = new Scene(root);
+			;
+			Stage primaryStage = (Stage) backButton.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
