797cd61fb3c09666b0947b146827e2c5c3d9fd66
Isaac-Lee <yy0221ss@gmail.com>
2019-12-04 00:08:11 +0900

    클라이언트 마무리

--- Connect_to_MariaDB/src/Client/java/ClientMainController.java
+++ Connect_to_MariaDB/src/Client/java/ClientMainController.java
@@ -13,13 +13,13 @@ import javafx.stage.Stage;
 import javafx.scene.control.ListView;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 
-public class ClientMainController implements Initializable{
+public class ClientMainController implements Initializable {
 
-	@FXML
-	TextArea currentLocation;
 	@FXML
 	ListView<String> scooterListview;
 	@FXML
@@ -28,7 +28,6 @@ public class ClientMainController implements Initializable{
 	Label numOfScooter;
 
 	private ObservableList<String> scooterList;
-	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -36,11 +35,18 @@ public class ClientMainController implements Initializable{
 		// 필요한 메서드는 Client 클래스에 있음
 		scooterList = FXCollections.observableArrayList();
 		scooterListview.setItems(scooterList);
+		numOfScooter.setText(numOfScooter());
 	}
 
 	@FXML
 	public void selectScooter() {
 		// TODO 해당 스쿠터가 이용 가능 할때만 사용할 수 있게 바꿔야함
+
+		String selectScooter = scooterListview.getSelectionModel().getSelectedItem();
+		if (selectScooter == null) {
+			new Alert(Alert.AlertType.WARNING, "선택된 스쿠터가 없습니다.", ButtonType.CLOSE).show();
+			return;
+		}
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/Client/resource/ClientRunning.fxml"));
 			Scene scene = new Scene(root);
@@ -49,6 +55,11 @@ public class ClientMainController implements Initializable{
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public String numOfScooter() {
+		String number = "총" + ((Integer) scooterList.size()).toString() + " 대";
+		return number;
 	}
 
 }
