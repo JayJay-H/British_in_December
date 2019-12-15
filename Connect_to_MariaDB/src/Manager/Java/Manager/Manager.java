770a9c6cbfe0480ad74b353b3312d4d14087c12a
JayJay-H <0911aza@naver.com>
2019-12-15 21:22:32 +0900

    1. Manager클래스 오류 수정

--- Connect_to_MariaDB/src/Manager/Java/Manager/Manager.java
+++ Connect_to_MariaDB/src/Manager/Java/Manager/Manager.java
@@ -7,12 +7,15 @@ import Server_Class.MemberManagement.memberManagement;
 import Server_Class.ScooterManagement.scooterManagement;
 
 public class Manager {
+	
+	static scooterManagement scootermanagement = new scooterManagement();
+	static authManager authmanager = new authManager();
 	static boolean addScooter(String ID, String Location) {
 		return scooterManagement.addScooter(ID, Location);
 	}
 	
 	static String showScooterLocation(String ID) throws SQLException {
-		return scooterManagement.findScooterList();
+		return scootermanagement.findScooterList();
 	}
 	
 	static String getMemberList() throws SQLException {
@@ -28,6 +31,6 @@ public class Manager {
 	}
 	
 	static String authManager(String ID, String Password) {
-		return authManager.authenticateManager(ID, Password);
+		return authmanager.authenticateManager(ID, Password);
 	}
 }
